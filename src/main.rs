3ca4e3f88656b230e57f3a6223f9e37085716b97
Daniel Way <projects@danieldway.com>
2023-02-05 21:01:56 -0600

    feat(main): implement basic map and render

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+#[derive(Copy, Clone)]
+enum Cell {
+    Empty, Wall, Goal
+}
+
+const SIZE: usize = 10;
+
 fn main() {
-    println!("Hello, world!");
+    let mut map = [[Cell::Empty; SIZE]; SIZE];
+    map[1][2] = Cell::Wall;
+    render(&map);
+}
+
+fn render(map: &[[Cell; SIZE]; SIZE]) {
+    println!("Map:");
+    for row in map {
+        for cell in row {
+            print!("{}", match cell {
+                Cell::Empty => " ",
+                Cell::Wall => "#",
+                Cell::Goal => "!",
+            });
+        }
+        println!();
+    }
 }
