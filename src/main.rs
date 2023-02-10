024e1864d3263bb988cae59fd439fcbee32c64fe
Daniel Way <projects@danieldway.com>
2023-02-10 15:23:46 -0800

    feat: implement plumbing of game loop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::io::stdout;
+use std::{io::stdout, thread::sleep, time::Duration};
 
+use rand::{rngs::ThreadRng, thread_rng, Rng};
 use tty_interface::{pos, Interface, Position, Result};
 
 mod layer;
@@ -14,40 +15,116 @@ enum Cell {
     Goal,
 }
 
+const SIZE: usize = 10;
+type Map = [[Cell; SIZE]; SIZE];
+
 struct Entity {
-    pos: (usize, usize),
+    position: (usize, usize),
 }
 
-const SIZE: usize = 10;
+impl Entity {
+    fn new(pos: (usize, usize)) -> Entity {
+        Entity { position: pos }
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+enum Move {
+    Up, Down, Left, Right
+}
+
+trait ControlledEntity {
+    fn get_move(&mut self, pos: (usize, usize), map: &Map) -> Option<Move>;
+}
+
+struct AIEntity {
+    rng: ThreadRng,
+}
+
+impl AIEntity {
+    fn new() -> AIEntity {
+        AIEntity {
+            rng: thread_rng(),
+        }
+    }
+}
+
+impl ControlledEntity for AIEntity {
+    fn get_move(&mut self, _pos: (usize, usize), _map: &Map) -> Option<Move> {
+        Some(match self.rng.gen_range(0..=4) {
+            0..=1 => Move::Up,
+            1..=2 => Move::Down,
+            2..=3 => Move::Left,
+            _ => Move::Right
+        })
+    }
+}
+
+const ITER_LIMIT: usize = 10;
 
 fn main() {
     execute().unwrap();
 }
 
 fn execute() -> Result<()> {
-    let map = generate();
-    let entity = Entity { pos: (0, 0) };
+    let map = generate_map();
+    
+    let mut entity = Entity::new((1, 1));
+    let mut ai = AIEntity::new();
 
     let mut stdout = stdout();
     let mut interface = Interface::new_relative(&mut stdout)?;
 
-    render(&mut interface, &map, &entity)?;
+    for _ in 0..ITER_LIMIT {
+        let entity_move = ai.get_move(entity.position, &map);
+
+        if let Some(entity_move) = entity_move {
+            match entity_move {
+                Move::Up => {
+                    if entity.position.1 > 1 {
+                        entity.position.1 -= 1;
+                    }
+                }
+                Move::Down => {
+                    if entity.position.1 < 8 {
+                        entity.position.1 += 1;
+                    }
+                }
+                Move::Left => {
+                    if entity.position.0 > 1 {
+                        entity.position.0 -= 1;
+                    }
+                }
+                Move::Right => {
+                    if entity.position.0 < 8 {
+                        entity.position.0 += 1;
+                    }
+                }
+            }
+        }
+
+        interface.clear_line(0);
+        interface.set(pos!(0, 0), &format!("{:?}", entity_move));
+        
+        render(&mut interface, &map, &entity)?;
+        sleep(Duration::from_millis(250));
+    }
 
     Ok(())
 }
 
-fn generate() -> [[Cell; SIZE]; SIZE] {
+fn generate_map() -> [[Cell; SIZE]; SIZE] {
     let mut map = [[Cell::Empty; SIZE]; SIZE];
 
-    for i in 2..9 {
-        map[3][i] = Cell::Wall;
+    for x in 0..SIZE {
+        for y in 0..SIZE {
+            if x == 0 || y == 0 || x == SIZE-1 || y == SIZE-1 {
+                map[y][x] = Cell::Wall;
+            }
+        }
     }
 
-    for i in 0..6 {
-        map[7][i] = Cell::Wall;
-    }
-
-    map[9][2] = Cell::Goal;
+    map[8][7] = Cell::Goal;
 
     map
 }
@@ -55,13 +132,13 @@ fn generate() -> [[Cell; SIZE]; SIZE] {
 fn render(interface: &mut Interface, map: &[[Cell; SIZE]; SIZE], entity: &Entity) -> Result<()> {
     for (y, row) in map.iter().enumerate() {
         for (x, cell) in row.iter().enumerate() {
-            if x == entity.pos.0 && y == entity.pos.1 {
-                interface.set(pos!(x as u16, y as u16), "E");
+            if x == entity.position.0 && y == entity.position.1 {
+                interface.set(pos!(x as u16, 1 + y as u16), "E");
                 continue;
             }
 
             interface.set(
-                pos!(x as u16, y as u16),
+                pos!(x as u16, 1 + y as u16),
                 match cell {
                     Cell::Empty => " ",
                     Cell::Wall => "#",
